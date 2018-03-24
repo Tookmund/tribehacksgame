e6569ef8b3bed4bc5a561a34c504c182f6538870
Jacob Adams <tookmund@gmail.com>
2018-03-24 14:35:22 -0400

    main: Randomly generate rectangles and check if the player is stuck

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 # Main Window
 
 from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication, QWidget
-from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
+from PyQt5.QtCore import Qt, QBasicTimer
 from PyQt5.QtGui import QPainter, QColor
-import sys
+import sys, random
 
 from Shape import Rect
 from Player import Player
+from Text import Text
+
 class Window(QMainWindow):
 	def __init__(self):
 		super().__init__()
@@ -23,26 +25,38 @@ class Frame(QWidget):
 		self.objects = [self.player]
 		self.grabKeyboard()
 		self.setGeometry(0, 0, w, h)
+		self.timer = QBasicTimer()
+		self.stuckTimer = QBasicTimer()
 		self.show()
+		self.timer.start(500,self)
+		self.stuckTimer.start(2000,self)
 
 	def paintEvent(self, event):
 		qp = QPainter(self)
 		for obj in self.objects:
 			obj.paint(qp)
 
+	def timerEvent(self, event):
+		self.objects.append(Rect(random.randint(0,649),random.randint(0,481)))
+		if event.timerId() == 2:
+			if not self.player.move(0,0,self.objects):
+				self.objects.append(Text(self.width()/2,self.height()/2,"You Deaded"))
+		self.repaint()
+
 	def keyPressEvent(self, event):
 		d = 10
 		if event.key() == Qt.Key_W:
-			self.player.move(0,-d)
+			self.player.move(0,-d,self.objects)
 		elif event.key() == Qt.Key_S:
-			self.player.move(0,d)
+			self.player.move(0,d,self.objects)
 		elif event.key() == Qt.Key_A:
-			self.player.move(-d,0)
+			self.player.move(-d,0,self.objects)
 		elif event.key() == Qt.Key_D:
-			self.player.move(d,0)
+			self.player.move(d,0,self.objects)
 		self.repaint()
+
+
 if __name__ == "__main__":
 	app = QApplication([])
 	win = Window()
-	win.create()
 	sys.exit(app.exec_())
