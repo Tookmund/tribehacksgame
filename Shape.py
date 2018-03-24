1d5f9079e53d5b5c1cac80df3a4f6065e9f54055
Jacob Adams <tookmund@gmail.com>
2018-03-24 14:35:59 -0400

    shape: if arguements are missing, randomly generate them
    can't do it directly in keyword because then the functions are only called once and stored for the future

--- Shape.py
+++ Shape.py
@@ -1,13 +1,25 @@
 from PyQt5.QtGui import QPainter, QColor, QPen 
+import random
 
 # Shapes
 from Object import Object
 
+def randomcolor():
+	return QColor(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+
 class Rect(Object):
-	def __init__(self, x, y, width, height, outline, fill):
+	def __init__(self, x, y, width=None, height=None, outline=None, fill=None):
 		super().__init__(x, y)
+		if width is None:
+			width = random.randint(10,100)
 		self.width = width
+		if height is None:
+			height = random.randint(10,100)
 		self.height = height
+		if outline is None:
+			outline = randomcolor()
+		if fill is None:
+			fill = randomcolor()
 		self.outline = QPen(outline)
 		self.fill = QColor(fill)
 
