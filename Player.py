30a770e375df30bf9637d5b91c4d5792d8d6be72
Jacob Adams <tookmund@gmail.com>
2018-03-24 12:47:12 -0400

    Add player movement

--- Player.py
+++ Player.py
@@ -1,20 +1,20 @@
-from PyQt5 import QtCore  
+from PyQt5 import QtCore
+from PyQt5.QtGui import QPainter, QColor, QPen
 from Object import Object
 
 class Player(Object):
 
 	def __init__(self, x, y):
 		super().__init__(x,y)
-		self.size = 50
+		self.size = 100
+		self.outline = QPen(QtCore.Qt.black)
+		self.fill = QColor(QtCore.Qt.red)
 
 	def paint(self, qp):
+		qp.setPen(self.outline)
+		qp.setBrush(self.fill)
 		qp.drawRect(self.x,self.y,self.size,self.size)
 
 	def move(self,dx,dy):
 		self.x += dx
 		self.y += dy
-	
-	def keypress(self, key):
-		if key == QtCore.Qt.Key_W:
-			print("w")
-		print(key)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication, QWidget
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
-from PyQt5.QtGui import QPainter, QColor 
+from PyQt5.QtGui import QPainter, QColor
 import sys, random
 
 from Shape import Rect
@@ -13,27 +13,37 @@ class Window(QMainWindow):
 		super().__init__()
 		self.resize(640,480)
 		self.setWindowTitle("GAME")
-		self.frame = Frame(self)
+		self.frame = Frame(self, 640, 480)
 		self.show()
 
 class Frame(QWidget):
-	def __init__(self, parent):
+	def __init__(self, parent, w, h):
 		super().__init__(parent)
-		self.player = Player(50,50)
+		self.player = Player(0,0)
 		self.objects = [self.player]
 		self.grabKeyboard()
+		self.setGeometry(0, 0, w, h)
 		self.show()
 
 	def paintEvent(self, event):
 		qp = QPainter(self)
-		qp.begin(self)
+		#qp.begin(self)
 		for obj in self.objects:
 			obj.paint(qp)
-		qp.end()
+		#qp.end()
+		print(self.player.x, " ", self.player.y)
 
 	def keyPressEvent(self, event):
-		self.player.keypress(event.key())
-
+		d = 10
+		if event.key() == Qt.Key_W:
+			self.player.move(0,-d)
+		elif event.key() == Qt.Key_S:
+			self.player.move(0,d)
+		elif event.key() == Qt.Key_A:
+			self.player.move(-d,0)
+		elif event.key() == Qt.Key_D:
+			self.player.move(d,0)
+		self.repaint()
 if __name__ == "__main__":
 	app = QApplication([])
 	win = Window()
